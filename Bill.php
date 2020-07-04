be1596dbe5650aaf522c5569760be2395dbdc320
reckonns <67047329+reckonns@users.noreply.github.com>
2020-07-04 11:39:11 +0530

    Merge pull request #4 from reckonns/np's
    Np's

---
+++ Bill.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bill</title>
+    
+</head>
+<body>
+
+<h1>This page is under constuction</h1>
+
+</body>
